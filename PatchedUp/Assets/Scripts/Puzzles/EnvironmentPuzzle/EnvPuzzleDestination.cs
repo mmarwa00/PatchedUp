bcbbf0cf8b559db1d082832f61d80d4d440a52b1
Benjamin Geiger <benjamin.geiger@stud.fh-campuswien.ac.at>
2026-05-23 19:45:14 +0200

    fixed PuzzleController bugs - puzzle works - puzzle flow needs work

---
+++ PatchedUp/Assets/Scripts/Puzzles/EnvironmentPuzzle/EnvPuzzleDestination.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class EnvPuzzleDestination : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- PatchedUp/Assets/Scripts/Puzzles/EnvironmentPuzzle/EnvironmentPuzzleEvents.cs
+++ PatchedUp/Assets/Scripts/Puzzles/EnvironmentPuzzle/EnvironmentPuzzleEvents.cs
@@ -6,22 +6,29 @@ public static class EnvironmentPuzzleEvents
 {
     public static Action<Light, bool, float, float> OnToggleLights;
 
-    public static Action<AudioClip, AudioMixerGroup, Vector3> OnEnvironmentSound;
+    public static Action<AudioClip, AudioMixerGroup, Vector3, bool> OnEnvironmentSound;
     
     public static Action<ParticleSystem, bool> OnEnvironmentParticles;
+
+    public static Action<AudioClip, AudioMixerGroup, Vector3> OnPuzzleSuccess;
 
     public static void ToggleLights(Light light, bool on, float minIntensity, float maxIntensity)
     {
         OnToggleLights?.Invoke(light, on,  minIntensity, maxIntensity);
     }
 
-    public static void EnvironmentSound(AudioClip clip, AudioMixerGroup mixerGroup, Vector3 position)
+    public static void EnvironmentSound(AudioClip clip, AudioMixerGroup mixerGroup, Vector3 position, bool isPlaying)
     {
-        OnEnvironmentSound?.Invoke(clip, mixerGroup, position);
+        OnEnvironmentSound?.Invoke(clip, mixerGroup, position,  isPlaying);
     }
 
     public static void EnvironmentParticles(ParticleSystem particles, bool isOn)
     {
         OnEnvironmentParticles?.Invoke(particles, isOn);
+    }
+
+    public static void PuzzleSuccess(AudioClip clip, AudioMixerGroup mixerGroup, Vector3 position)
+    {
+        OnPuzzleSuccess?.Invoke(clip, mixerGroup, position);
     }
 }
